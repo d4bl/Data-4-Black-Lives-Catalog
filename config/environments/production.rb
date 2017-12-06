f45b4473c417de42826d8e954f157eefcec6c0d8
Beckah Sheeler <nunyabiz@gmail.com>
2017-12-06 16:35:28 -0500

    configured sendgrid options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'http://socifyapp.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://still-fortress-85980.herokuapp.com/' }
 
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.perform_deliveries = true
