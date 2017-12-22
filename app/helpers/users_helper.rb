d0ed8af6937edbdf63d707ebd000eea3dc74d450
Beckah Sheeler <nunyabiz@gmail.com>
2017-12-22 11:11:21 -0500

    Removed comments. Attributing credit in README.md

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
-# Copyright (c) 2015, @sudharti(Sudharsanan Muralidharan)
-# Socify is an Open source Social network written in Ruby on Rails This file is licensed
-# under GNU GPL v2 or later. See the LICENSE.
+
+
+
 
 module UsersHelper
   def options_for_seasons
---
+++ db/migrate/20171222160749_remove_attachment_from_post.rb
@@ -0,0 +1,4 @@
+class RemoveAttachmentFromPost < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
